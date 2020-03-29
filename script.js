3a52fce9230edf7738b086833e4714d3b9c5641e
Kristjan Fale <kristjan.fale@gmail.com>
2020-03-29 16:25:28 +0200

    JS Menu Slide & JS Modal Toggle

--- script.js
+++ script.js
@@ -1,1 +1,26 @@
+const toggle = document.getElementById('toggle');
+const open = document.getElementById('open');
+const close = document.getElementById('close');
+const modal = document.getElementById('modal-container');
 
+// Toggle nav
+toggle.addEventListener('click', () => {
+  document.body.classList.toggle('nav-open');
+});
+
+// Show modal
+open.addEventListener('click', () => {
+  modal.classList.add('modal-open');
+});
+
+// Close modal
+close.addEventListener('click', () => {
+  modal.classList.remove('modal-open');
+});
+
+// Close modal on outside click
+window.addEventListener('click', e => {
+  if (e.target.id == 'modal-container') {
+    modal.classList.remove('modal-open');
+  }
+});
